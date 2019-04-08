475885845cc0a84f580f71773be8c249464d1784
Reese Anspaugh <reese.anspaugh@gmail.com>
2019-04-08 21:30:21 +0000

    Changed a bit of the code, nothing to big

--- static/js/custom.js
+++
@@ -1,8 +0,0 @@
-function addPink(){
-     var statusText = document.querySelector('#statusText');
-    statusText.classList.add('pink-border');
-}
-
-statusText.addEventListener("mouseover", function(){
-    function addPink();
-});
